c2ccaa8221107a0d9f22837f38835de082c62403
Elias (Eli) Dow <56176047+elidow@users.noreply.github.com>
2026-01-09 16:01:25 -0500

    Fix callback issue on develop branch (#33)

--- sound-check/src/App.js
+++ sound-check/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar/index.js";
 import PageFooter from './components/common/PageFooter.js';
-import PlaylistsPage from './pages/PlaylistsPage.js'
-import DashboardsPage from './pages/DashboardsPage.js'
-import TablesPage from './pages/TablesPage.js'
-import UserPage from './pages/UserPage.js'
+import PlaylistsPage from './pages/PlaylistsPage.js';
+import DashboardsPage from './pages/DashboardsPage.js';
+import TablesPage from './pages/TablesPage.js';
+import UserPage from './pages/UserPage.js';
+import CallbackPage from './pages/CallbackPage.js';
 import './App.css';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                         <Route path="/tables" element={<TablesPage />}></Route>
                         <Route path="/user" element={<UserPage />}></Route>
                         <Route path="*" element={<Navigate to="/playlists" replace />} />
+                        <Route path="/callback.html" element={<CallbackPage />} />
                     </Routes>
                 </div>
                 <PageFooter />
